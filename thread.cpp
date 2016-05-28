c1395c7b7d27b994b64811a1b3afc8ab6f565a08
Reupen Shah <reupen@users.noreply.github.com>
2016-05-28 12:25:45 +0100

    Count cores with separate functional units as distinct cores

--- thread.cpp
+++ thread.cpp
@@ -3,8 +3,12 @@
 namespace mmh {
 	size_t get_cpu_core_count()
 	{
+		struct CoreInfo {
+			size_t mask;
+			uint8_t flags;
+		};
 		static bool initialised;
-		static pfc::array_t<size_t> core_masks;
+		static pfc::array_t<CoreInfo> core_info;
 
 		if (!initialised) {
 			DWORD slpi_array_size = 0;
@@ -22,26 +26,41 @@ namespace mmh {
 							core_count++;
 					}
 
-					core_masks.set_size(core_count);
+					core_info.set_size(core_count);
 
 					for (t_size i = 0, j = 0; i < slpi_count; i++) {
 						const auto & slpi = slpis[i];
-						if (slpi.Relationship == RelationProcessorCore)
-							core_masks[j++] = slpi.ProcessorMask;
+						if (slpi.Relationship == RelationProcessorCore) {
+							auto & item = core_info[j++];
+							item.mask = slpi.ProcessorMask;
+							item.flags = slpi.ProcessorCore.Flags;
+						}
 					}
 				}
 			}
 			initialised = true;
 		}
 
-		DWORD mask_process = 0, mask_system = 0;
+		DWORD_PTR mask_process = 0, mask_system = 0;
 		GetProcessAffinityMask(GetCurrentProcess(), &mask_process, &mask_system);
 
-		t_size cpu_count = 0, core_mask_count = core_masks.get_count();
-		for (t_size i = 0; i < core_mask_count; i++) {
-			if (mask_process & core_masks[i])
-				cpu_count++;
+		t_size core_count = 0, core_info_count = core_info.get_count();
+		for (t_size i = 0; i < core_info_count; i++) {
+			const auto & item = core_info[i];
+			// MSDN: If the value of this member is 1, the logical processors identified by the value of 
+			// the ProcessorMask member share functional units, as in Hyperthreading or SMT. Otherwise, 
+			// the identified logical processors do not share functional units.
+			if (item.flags == 1) {
+				if (mask_process & item.mask)
+					core_count++;
+			} else {
+				size_t intersection = mask_process & item.mask;
+				while (intersection) {
+					if (intersection & 1) core_count++;
+					intersection >>= 1;
+				}
+			}
 		}
-		return cpu_count > 0 ? cpu_count : 1;
+		return core_count > 0 ? core_count : 1;
 	}
 }
