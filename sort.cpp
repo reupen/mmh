897d70ce05b66fe91709eb063b0feb11a28c9426
Reupen Shah <reupen@users.noreply.github.com>
2017-07-01 12:29:39 +0100

    Remove unused g_compare_context_lt() function

--- sort.cpp
+++ sort.cpp
@@ -6,9 +6,4 @@ namespace mmh
 	{
 		return ((sort_base*)(context))->compare(item1, item2);
 	}
-
-	int g_compare_context_lt(void* context, const void * item1, const void* item2)
-	{
-		return ((sort_base*)(context))->compare(item1, item2)<0;
-	}
 }
